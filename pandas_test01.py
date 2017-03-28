0fbb4c1d9ffe298e5461e179fa38bc1d923ca65b
jesus4000 <jesus7743@gmail.com>
2017-03-28 18:02:50 +0900

    new

--- pandas_test01.py
+++ pandas_test01.py
@@ -1,5 +1,11 @@
 import pymssql
-import pandas_datareader.data as web
+import pandas_datareader.data as pd
+import datetime
+
+today = datetime.datetime.now().strftime('%Y%m%d')
+yesterday = (datetime.datetime.now() + datetime.timedelta(days=-8)).strftime('%Y%m%d')
+
+
 
 def shcode_load(db_adr,id,pw,db_name):
     global shcode_list
@@ -25,25 +31,74 @@ def shcode_load(db_adr,id,pw,db_name):
         print('DB접속정보 확인요망')
 
 
-print(shcode_load('localhost','jesus','3477','test'))
-
-
-# def pandas_load():
-START = "2016-03-07"
-END =  "2016-03-10"
-shcode_list = '000020'
-kospi = web.DataReader("KRX:" + shcode_list, "google", START, END)
-
-date  = list(kospi.index.strftime('%Y%m%d'))
-open  = list(kospi['Open'])
-high  = list(kospi['High'])
-low   = list(kospi['Low'])
-close = list(kospi['Close'])
-vol   = list(kospi['Volume'])
+# print(shcode_load('localhost','jesus','3477','test'))
 
 
 
-# print(kospi.index)
+
+
+
+#################################
+
+# def pandas_load(start,end):
+
+shcode_load('localhost','jesus','3477','test')
+
+start = ''
+end = ''
+
+if(start == ''):
+    start = yesterday
+if(end == ''):
+    end = today
+
+
+## for 문 시작
+pd_data = ()
+
+for i in range(len(shcode_list)):
+    pd_data = pd.DataReader("KRX:" + shcode_list[i], "google", start, end)
+    date  = list(pd_data.index.strftime('%Y%m%d'))
+    open  = list(pd_data['Open'])
+    high  = list(pd_data['High'])
+    low   = list(pd_data['Low'])
+    close = list(pd_data['Close'])
+    vol   = list(pd_data['Volume'])
+
+    pd_list = [date,open, high, low, close, vol]
+
+
+
+
+    # for j in range(len(date)):
+    #     date[j]
+
+
+
+
+print(shcode_list[0])
+print(pd_list)
+
+test = []
+test2 = []
+
+for a in range(len(date)):
+    for b in range(6):    #date ~ vol 까지 컬럼
+        test.append(pd_list[b][a])
+        test2 = test2,test
+
+
+# print(pd_list[0][0])
+print(test)
+print(test2)
+
+
+
+    # return x
+
+
+
+# print(pd_data.index)
 #
 # target_list = []
 #
